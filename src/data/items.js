900ba97522b95679eeba597bcc6ea9c36a39734d
Achilles-one <2276635831@qq.com>
2021-09-03 21:17:34 +0800

    master

---
+++ src/data/items.js
@@ -0,0 +1,39 @@
+const items = [
+    {
+        id: 1,
+        name: "拖鞋",
+        price: 12.00,
+        category: 'Footwear',
+        description: "Achilles caught up with the tortoise and sat comfortably on its back."
+    },
+    {
+        id: 2,
+        name: "裤子",
+        price: 56.00,
+        category: 'body',
+        description: "Achilles caught up with the tortoise and sat comfortably on its back."
+    },
+    {
+        id: 3,
+        name: "衣服",
+        price: 79.00,
+        category: 'body',
+        description: "Achilles caught up with the tortoise and sat comfortably on its back."
+    },
+    {
+        id: 4,
+        name: "帽子",
+        price: 29.00,
+        category: 'head',
+        description: "Achilles caught up with the tortoise and sat comfortably on its back."
+    },
+    {
+        id: 5,
+        name: "袜子",
+        price: 8.00,
+        category: 'Footwear',
+        description: "Achilles caught up with the tortoise and sat comfortably on its back."
+    },
+]
+
+export default items
---
+++ src/views/Cart.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>Your Cart</h1>
+
+    <CartItemCard 
+    v-for="product in products"
+    :key="product.id"
+    :product="product"
+    />
+    
+    <!-- <div v-for="product in items" :key="product.id">
+      {{ product.name }}
+    </div> -->
+
+    <CartSummaryPaymentCard />
+
+  </div>
+</template>
+
+
+
+<script>
+import CartItemCard from '../components/cart/CartItemCard.vue'
+import CartSummaryPaymentCard from '../components/cart/CartSummaryPaymentCard.vue'
+
+export default {
+  components: {
+    CartItemCard,CartSummaryPaymentCard
+  },
+  computed: {
+    products() {
+      return this.$store.getters.cartItems
+    }
+  }
+}
+</script>
